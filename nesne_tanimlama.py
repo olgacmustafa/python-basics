48785bf167cbb1f7f00867e90c51f5d1a2f0ac68
olgacmustafa <olgcmustafa@gmail.com>
2026-03-29 09:15:58 +0300

    upload file

---
+++ nesne_tanimlama.py
@@ -0,0 +1,33 @@
+print("Mağazamıza Hoşgeldiniz!")
+isim=input("İsminizi giriniz: ")
+soyIsim=input("Soyisminizi giriniz: ")
+telefon=input("Telefon numaranızı giriniz: ")
+mail=input("mail adresinizi giriniz: ")
+yas=int(input("yasınızı giriniz: "))
+print("Kayıt işleminiz başarıyla tamamlanmıştır.")  
+
+print("Bigileriniz:")
+print("İsim Soyisim:", isim, soyIsim)
+print("Telefon:", telefon)
+print("Mail:", mail)
+print("Yaş:", yas)
+
+#ürünler
+mouse="Kablosuz Mouse"
+mouse_fiyat=550
+klavye="Kablosuz Klavye"
+klavye_fiyat=650
+laptop="Dizüstü Bilgisayar"
+laptop_fiyat=55000
+
+print ("Satin alinan ürünler")
+
+print("Ürün adi=",mouse)
+print("Fiyati=",mouse_fiyat)
+
+print("Ürün adi=",klavye)
+print("Fiyati=", klavye_fiyat)
+
+print("Ürün adi=",laptop)
+print("Fiyati", laptop_fiyat)
+
