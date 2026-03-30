9d815ae9c20a13d0772126b1ec2f6583ba891caa
olgacmustafa <olgcmustafa@gmail.com>
2026-03-31 01:58:11 +0300

    upload file

--- basit_islemler.py
+++ basit_islemler.py
@@ -1,3 +1,9 @@
-print("Dairenin çapını giriniz")
-cap=int(input())
-print("Dairenin Alanı:"+"pi x",(cap/2)*(cap/2))
+cap=int(input("Dairenin çapını giriniz: "))
+r=cap/2
+pi=3.14
+alan=pi*(r**2)
+cevre=2*pi*r
+cevre=round(cevre , 2)
+alan=round(alan , 2)
+print("Dairenin Alanı: ",alan )
+print ("Dairenin Çevresi: ",cevre)
