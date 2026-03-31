91e5c3897753504a3412835b0d7e762d930ca091
olgacmustafa <olgcmustafa@gmail.com>
2026-03-31 20:28:44 +0300

    upload file

---
+++ bolum-4/aritmetik_operatorler.py
@@ -0,0 +1,13 @@
+a=30
+b=20
+
+
+sonuc= a+b      #toplama
+sonuc= a-b      #çıkarma
+sonuc= a*b      #çarpma
+sonuc= a/b      #bolme
+sonuc= a%b      #mod_alma
+sonuc= a**b     #üs_alma
+sonuc= a//b     #tam_bölüm
+
+print(sonuc)
