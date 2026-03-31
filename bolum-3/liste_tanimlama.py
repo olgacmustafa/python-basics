249909ac6906f0a48eb87239347898565fa5131c
olgacmustafa <olgcmustafa@gmail.com>
2026-03-31 16:06:11 +0300

    file upload

---
+++ bolum-3/liste_tanimlama.py
@@ -0,0 +1,25 @@
+programlama_dilleri=["Python","C#","Java","Javascript"]
+
+sonuc= programlama_dilleri
+sonuc=programlama_dilleri[0:2]
+
+
+#güncelleme
+
+sonuc=programlama_dilleri[-1]="PHP"
+
+sonuc=programlama_dilleri
+
+sonuc =len(programlama_dilleri)
+sonuc=programlama_dilleri+["Go","Delphi"]
+
+sonuc= "Python" in programlama_dilleri
+sonuc= "react" in programlama_dilleri
+
+del programlama_dilleri[0]
+
+for dil in programlama_dilleri: 
+    print(dil)
+
+print(sonuc)
+
