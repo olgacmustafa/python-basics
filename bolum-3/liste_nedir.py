c1fb495665b3d07044884f7c81bcadcd476ee6f6
olgacmustafa <olgcmustafa@gmail.com>
2026-03-31 15:52:24 +0300

    upload file

---
+++ bolum-3/liste_nedir.py
@@ -0,0 +1,27 @@
+kurum="BTK AKADEMİ".split()
+
+print(kurum)
+print(type(kurum))
+print(kurum[0]+kurum[1])
+
+sayilar = [1,2,3,4,5]
+isimler= ["ali","ahmet","ayşe"]
+
+print(type(sayilar))
+print(type(sayilar[0]))
+
+print(type(isimler))
+print(type(isimler[1]))
+
+ogrenci = ["Çınar","Turan",60,50,70]
+print(ogrenci[0] + " " + ogrenci[1])
+
+ortalama= (ogrenci[2] + ogrenci[3] +ogrenci[4]) /3
+print(ortalama)
+
+ogrenciler= [["Çınar","Turan",60,50,70],["Ali","Turan",40,80,70]]
+
+print(ogrenciler[0])
+print(ogrenciler[0][1])
+
+print(ogrenciler[1])
