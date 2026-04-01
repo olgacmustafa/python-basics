5fab4d8fd490ec231d0c3da28a48b4baa08792cc
olgacmustafa <olgcmustafa@gmail.com>
2026-04-01 11:52:34 +0300

    upload file

---
+++ bolum-4/karsilastirma_operatorleri.py
@@ -0,0 +1,11 @@
+a,b,c,d=2,2,10,5
+eposta= "info@olgcmustafa.com"
+parola= "12345"
+
+sonuc = (a == b)
+sonuc = (a != b)
+
+sonuc = (eposta == input("eposta: "))
+sonuc = (parola== input("parola: "))
+
+print(sonuc)
