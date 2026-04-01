c234c37ea42d3e91fc49e66e5535d5d54e86d6d7
olgacmustafa <olgcmustafa@gmail.com>
2026-04-01 16:56:38 +0300

    upload file

---
+++ bolum-6/for_dongusu.py
@@ -0,0 +1,17 @@
+sayilar=[1,2,3,4,5,6,7,8]
+
+for x in sayilar:
+    print(x)
+
+for x in sayilar:
+    print("Merhaba Mustafa")
+
+my_tuple=[(1,2),(3,4),(5,6)] 
+
+for i in my_tuple:
+    print(i)
+
+my_dict= {"41" : "Kocaeli",  "53":"Rize"}
+
+for x in my_dict:
+    print(x,my_dict[x]);
