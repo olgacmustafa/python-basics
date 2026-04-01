bc03e1de1b147ba9f25eebf993ccd7d82e1abd0a
olgacmustafa <olgcmustafa@gmail.com>
2026-04-01 13:53:46 +0300

    upload file

---
+++ bolum-5/if-else.py
@@ -0,0 +1,11 @@
+email="info@olgcmustafa.com"
+parola="12345"
+
+
+if(email=="info@olgcmustafa.com"):
+    if(parola=="12345"):
+        print("Login olundu")
+    else:
+        print("Parola yanlış")
+else:
+    print("Email yanlış")
